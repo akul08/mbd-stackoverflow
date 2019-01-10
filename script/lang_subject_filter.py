de1be9bb01ec6ca77c447ea101b45571bc9582de
Akul Mehra <mehra.akul@gmail.com>
2019-01-10 17:55:00 +0100

    use sample3, make popularity function to generate both year and month data separately

--- script/lang_subject_filter.py
+++ script/lang_subject_filter.py
@@ -8,30 +8,47 @@ sc = SparkContext()
 sc.setLogLevel("Error")
 spark = SparkSession.builder.getOrCreate()
 
+def calculate_popularity_save_csv(subset, csv_path, lang, subject, group_column_list, filename_extension):
+	# printing the current arguments
+	print 'write: ', csv_path, lang, subject, group_column_list, filename_extension 
+        
+	# count no. of questions for each group_column_list
+        df_ques_count = subset.groupBy(*group_column_list).count()
+        df_ques_count.repartition(1).write.mode('overwrite').csv(csv_path+subject+'/'+lang+'_'+'ques_count_'+filename_extension)
+	
+        # count total views of questions for each group_column_list
+        df_views_count = subset.groupBy(*group_column_list).agg({'_ViewCount': 'sum'})
+        df_views_count.repartition(1).write.mode('overwrite').csv(csv_path+subject+'/'+lang+'_'+'views_count_'+filename_extension)
+
+        # count total answers of questions for each group_column_list
+        df_answers_count = subset.groupBy(*group_column_list).agg({'_AnswerCount': 'sum'})
+        df_answers_count.repartition(1).write.mode('overwrite').csv(csv_path+subject+'/'+lang+'_'+'answers_count_'+filename_extension)
+
+        # count total score of questions for each group_column_list
+        df_score_count = subset.groupBy(*group_column_list).agg({'_Score': 'sum'})
+        df_score_count.repartition(1).write.mode('overwrite').csv(csv_path+subject+'/'+lang+'_'+'score_count_'+filename_extension)
+
+
 def subset_func(subset, csv_path, lang, subject):
 	print(subset.count())
 	#result = subset.select('_Tags').rdd.map(lambda x: x.replace('>','').replace('<',' ')).flatMap(lambda x: x.split(' ')).map(lambda x: (x,1).reduceByKey(lambda a,b: a+b))
 	#res = result.sortBy(lambda x: x[1], ascending=False)
 	#print(res.collect())
-	# count no. of questions for each year
-	df_ques_count = subset.groupBy(subset.year).count()
-	df_ques_count.repartition(1).write.csv(csv_path+subject+'/'+lang+'_'+'ques_count.csv')
 
-	# count total views of questions for each year
-	df_views_count = subset.groupBy(subset.year).agg({'_ViewCount': 'sum'})
-	df_views_count.repartition(1).write.csv(csv_path+subject+'/'+lang+'_'+'views_count.csv')
+	# to remove the angled brackets from lang and subject
+	lang = lang[1:-1]
+	subject = subject[1:-1]
 
-	# count total answers of questions for each year
-	df_answers_count = subset.groupBy(subset.year).agg({'_AnswerCount': 'sum'})
-	df_answers_count.repartition(1).write.csv(csv_path+subject+'/'+lang+'_'+'answers_count.csv')
+        # For Year aggregation
+        # Note: passing tuple to group_column_list variable
+        calculate_popularity_save_csv(subset, csv_path, lang, subject, ('year', ), 'year')
 
-	# count total score of questions for each year
-	df_score_count = subset.groupBy(subset.year).agg({'_Score': 'sum'})
-	df_score_count.repartition(1).write.csv(csv_path+subject+'/'+lang+'_'+'score_count.csv')
+        # For year and month aggregation
+        calculate_popularity_save_csv(subset, csv_path, lang, subject, ('year', 'month'), 'year_month')
 
 
 if __name__ == "__main__":
-	data_path = "file:///home/s1745646/Project/sample2"
+	data_path = "file:///home/s2118947/sample3"
 	csv_path = 'file:///home/s2118947/'
 
 	if len(sys.argv) > 1:
